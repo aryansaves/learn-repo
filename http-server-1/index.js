2a90a2c1563b4369273d07a33cfa35973224a45a
Aryan <aryansrivastava354@gmail.com>
2025-09-11 01:35:10 +0530

    uptill auth, --jwt remains

--- http-server-1/index.js
+++ http-server-1/index.js
@@ -1,19 +1,59 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.json())
+const express = require("express")
+const app = express() 
+const jwt  = require("jsonwebtoken")
+const jwtpass = "mysecret"
 
 
-app.post('/gooncave', function (req, res) {
-    const json = req.body
-    console.log(json)
-    res.status(200).send("DONE!!")
+const user = [
+  {
+    username: "aryan@gmail.com",
+    password: "password1",
+    name : "Aryan"
+  },
+  {
+    username: "Aniket@gmail.com",
+    password: "password2",
+    name : "Anni"
+  },
+  {
+    username: "Aiman@duckduckgo.com",
+    password: "password3",
+    name : "Aiman"  
+  }
+]
+
+function userexists(username, password){
+
+}
+
+
+app.post("/signin", (req, res) => {
+const username = req.query.username
+const password = req.query.password
+
+if(!userexists(username, password)){
+  return res.status(403).json({
+    msg : "User does not exist"
+  })
+
+}
+var token = jwt.sign({ username: username }, "shhhhhh");
+  return res.json({
+  token,
+  })
 });
 
-app.get('/goonget',(req,res)=>{
-  const n = req.query.n
-  res.send(n*10)
-})
-app.listen(port, function()  {
-  console.log(`Example app listening on port ${port}`)
-})
+
+app.get("/users", (req, res) => {
+  const token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, jwtpass);
+    const username = decoded.username;
+  }
+  catch(err) {
+    return res.status(403).json({
+      msg : "Invalid token"
+    });
+  }
+  });
+app.listen(3000)
